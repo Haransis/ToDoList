4a687c7bbf0e3cab49aebb3b0a9a8152c8827257
Alban Rahier <ara.rahier@gmail.com>
2019-06-16 22:06:13 +0200

    Ajout de commentaire. Refactorisation du code.

--- app/src/main/java/com/example/myhello/data/ListeToDoServiceFactory.java
+++ app/src/main/java/com/example/myhello/data/ListeToDoServiceFactory.java
@@ -6,14 +6,19 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ListeToDoServiceFactory {
 
+    // L'Url de base.
     public static String Url = "http://tomnab.fr/todo-api/";
+
     public static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(Url)
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 
+    // Cette méthode est appelée lors d'un changement
+    // d'Url de base.
     public static void changeUrl(String url){
         Url = url;
+        // On a besoin de recréer l'instance retrofit.
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
